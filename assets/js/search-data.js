72c85921badc8aa503bf42f3a55c86d12c0a5fa0
sawhney-medha <sawhney.medha@gmail.com>
2025-01-31 23:29:34 +0000

    Deploying to gh-pages from @ sawhney-medha/sawhney-medha.github.io@e207463f6f0a82c11ccad0148d61cd25bc43af47 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,516 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "About",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-publications",
+          title: "Publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "post-a-post-with-image-galleries",
+      
+        title: "a post with image galleries",
+      
+      description: "this is what included image galleries could look like",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/photo-gallery/";
+        
+      },
+    },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
+      
+        title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+      
+      description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
+      section: "Posts",
+      handler: () => {
+        
+          window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
+        
+      },
+    },{id: "post-a-post-with-tabs",
+      
+        title: "a post with tabs",
+      
+      description: "this is what included tabs in a post could look like",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/tabs/";
+        
+      },
+    },{id: "post-a-post-with-typograms",
+      
+        title: "a post with typograms",
+      
+      description: "this is what included typograms code could look like",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/typograms/";
+        
+      },
+    },{id: "post-a-post-that-can-be-cited",
+      
+        title: "a post that can be cited",
+      
+      description: "this is what a post that can be cited looks like",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/post-citation/";
+        
+      },
+    },{id: "post-a-post-with-pseudo-code",
+      
+        title: "a post with pseudo code",
+      
+      description: "this is what included pseudo code could look like",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/pseudocode/";
+        
+      },
+    },{id: "post-a-post-with-code-diff",
+      
+        title: "a post with code diff",
+      
+      description: "this is how you can display code diffs",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/code-diff/";
+        
+      },
+    },{id: "post-a-post-with-advanced-image-components",
+      
+        title: "a post with advanced image components",
+      
+      description: "this is what advanced image components could look like",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/advanced-images/";
+        
+      },
+    },{id: "post-a-post-with-vega-lite",
+      
+        title: "a post with vega lite",
+      
+      description: "this is what included vega lite code could look like",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/vega-lite/";
+        
+      },
+    },{id: "post-a-post-with-geojson",
+      
+        title: "a post with geojson",
+      
+      description: "this is what included geojson code could look like",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/geojson-map/";
+        
+      },
+    },{id: "post-a-post-with-echarts",
+      
+        title: "a post with echarts",
+      
+      description: "this is what included echarts code could look like",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/echarts/";
+        
+      },
+    },{id: "post-a-post-with-chart-js",
+      
+        title: "a post with chart.js",
+      
+      description: "this is what included chart.js code could look like",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/chartjs/";
+        
+      },
+    },{id: "post-a-post-with-tikzjax",
+      
+        title: "a post with TikZJax",
+      
+      description: "this is what included TikZ code could look like",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2023/tikzjax/";
+        
+      },
+    },{id: "post-a-post-with-bibliography",
+      
+        title: "a post with bibliography",
+      
+      description: "an example of a blog post with bibliography",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2023/post-bibliography/";
+        
+      },
+    },{id: "post-a-post-with-jupyter-notebook",
+      
+        title: "a post with jupyter notebook",
+      
+      description: "an example of a blog post with jupyter notebook",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2023/jupyter-notebook/";
+        
+      },
+    },{id: "post-a-post-with-custom-blockquotes",
+      
+        title: "a post with custom blockquotes",
+      
+      description: "an example of a blog post with custom blockquotes",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2023/custom-blockquotes/";
+        
+      },
+    },{id: "post-a-post-with-table-of-contents-on-a-sidebar",
+      
+        title: "a post with table of contents on a sidebar",
+      
+      description: "an example of a blog post with table of contents on a sidebar",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2023/sidebar-table-of-contents/";
+        
+      },
+    },{id: "post-a-post-with-audios",
+      
+        title: "a post with audios",
+      
+      description: "this is what included audios could look like",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2023/audios/";
+        
+      },
+    },{id: "post-a-post-with-videos",
+      
+        title: "a post with videos",
+      
+      description: "this is what included videos could look like",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2023/videos/";
+        
+      },
+    },{id: "post-displaying-beautiful-tables-with-bootstrap-tables",
+      
+        title: "displaying beautiful tables with Bootstrap Tables",
+      
+      description: "an example of how to use Bootstrap Tables",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2023/tables/";
+        
+      },
+    },{id: "post-a-post-with-table-of-contents",
+      
+        title: "a post with table of contents",
+      
+      description: "an example of a blog post with table of contents",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2023/table-of-contents/";
+        
+      },
+    },{id: "post-a-post-with-giscus-comments",
+      
+        title: "a post with giscus comments",
+      
+      description: "an example of a blog post with giscus comments",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2022/giscus-comments/";
+        
+      },
+    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
+      
+        title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
+        
+      },
+    },{id: "post-a-post-with-redirect",
+      
+        title: "a post with redirect",
+      
+      description: "you can also redirect to assets like pdf",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/assets/pdf/example_pdf.pdf";
+        
+      },
+    },{id: "post-a-post-with-diagrams",
+      
+        title: "a post with diagrams",
+      
+      description: "an example of a blog post with diagrams",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2021/diagrams/";
+        
+      },
+    },{id: "post-a-distill-style-blog-post",
+      
+        title: "a distill-style blog post",
+      
+      description: "an example of a distill-style blog post and main elements",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2021/distill/";
+        
+      },
+    },{id: "post-a-post-with-twitter",
+      
+        title: "a post with twitter",
+      
+      description: "an example of a blog post with twitter",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2020/twitter/";
+        
+      },
+    },{id: "post-a-post-with-disqus-comments",
+      
+        title: "a post with disqus comments",
+      
+      description: "an example of a blog post with disqus comments",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2015/disqus-comments/";
+        
+      },
+    },{id: "post-a-post-with-math",
+      
+        title: "a post with math",
+      
+      description: "an example of a blog post with some math",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2015/math/";
+        
+      },
+    },{id: "post-a-post-with-code",
+      
+        title: "a post with code",
+      
+      description: "an example of a blog post with some code",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2015/code/";
+        
+      },
+    },{id: "post-a-post-with-images",
+      
+        title: "a post with images",
+      
+      description: "this is what included images could look like",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2015/images/";
+        
+      },
+    },{id: "post-a-post-with-formatting-and-links",
+      
+        title: "a post with formatting and links",
+      
+      description: "march &amp; april, looking forward to summer",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2015/formatting-and-links/";
+        
+      },
+    },{id: "news-books-joined-the-ms-in-computer-science-program-at-virginia-tech",
+          title: ':books: Joined the MS in Computer Science Program at Virginia Tech',
+          description: "",
+          section: "News",},{id: "news-trophy-honored-to-be-the-recipient-of-the-grace-hopper-celebration-ghc-2022-student-scholarship-awarded-by-cs-vt-and-anitab-org",
+          title: ':trophy: Honored to be the recipient of the Grace Hopper Celebration (GHC) 2022...',
+          description: "",
+          section: "News",},{id: "news-sparkles-interning-at-twitter-as-a-machine-learning-engineering-intern-this-summer",
+          title: ':sparkles: Interning at Twitter  as a Machine Learning Engineering Intern this Summer!',
+          description: "",
+          section: "News",},{id: "news-scroll-new-preprint-available-deep-learning-enabled-label-free-cell-force-computation-in-deformable-fibrous-environments-preprint",
+          title: ':scroll: New preprint available “Deep Learning Enabled Label-free Cell Force Computation in Deformable...',
+          description: "",
+          section: "News",},{id: "news-woman-student-embarked-on-a-ph-d-journey-at-virginia-tech",
+          title: ':woman_student: Embarked on a Ph.D. Journey at Virginia Tech!',
+          description: "",
+          section: "News",},{id: "news-scroll-excited-to-have-received-the-opportunity-to-present-my-work-on-bacteria-tracking-at-cvpr-2023-as-part-of-the-cv4animals-workshop-smile-poster-workshop-website",
+          title: ':scroll: Excited to have received the opportunity to present my work on Bacteria...',
+          description: "",
+          section: "News",},{id: "news-scroll-new-preprint-available-memtrack-a-deep-learning-based-approach-to-microrobot-tracking-in-dense-and-low-contrast-environments-preprint-code",
+          title: ':scroll: New preprint available “MEMTRACK: A Deep Learning-Based Approach to Microrobot Tracking in...',
+          description: "",
+          section: "News",},{id: "news-tada-our-paper-memtrack-a-deep-learning-based-approach-to-microrobot-tracking-in-dense-and-low-contrast-environments-was-accepted-to-advanced-intelligent-systems-paper-code",
+          title: ':tada: Our paper “MEMTRACK: A Deep Learning-Based Approach to Microrobot Tracking in Dense...',
+          description: "",
+          section: "News",},{id: "news-sparkles-i-ll-be-delivering-a-lightning-talk-at-the-imageomics-workshop-aaai-24-conference-in-vancouver-canada",
+          title: ':sparkles: I’ll be delivering a lightning talk at the Imageomics workshop, AAAI’24 Conference...',
+          description: "",
+          section: "News",},{id: "news-star-struck-excited-to-be-interning-at-nvidia-as-a-deep-learning-automation-intern-this-summer",
+          title: ':star_struck: Excited to be interning at NVIDIA as a Deep Learning Automation Intern...',
+          description: "",
+          section: "News",},{id: "news-scroll-new-preprint-available-a-unified-framework-for-forward-and-inverse-problems-in-subsurface-imaging-using-latent-space-translations-preprint",
+          title: ':scroll: New preprint available “A Unified Framework for Forward and Inverse Problems in...',
+          description: "",
+          section: "News",},{id: "news-rocket-excited-to-announce-that-our-paper-vlm4bio-a-new-benchmark-dataset-for-vision-language-models-has-been-accepted-to-neurips-2024-this-work-introduces-a-comprehensive-benchmark-tailored-for-evaluating-vision-language-models-in-the-context-of-biological-data-page-facing-up-paper-computer-code",
+          title: ':rocket: Excited to announce that our paper, “VLM4Bio: A New Benchmark Dataset for...',
+          description: "",
+          section: "News",},{id: "news-tada-i-finally-have-an-iclr-paper-thrilled-to-share-that-our-paper-a-unified-framework-for-forward-and-inverse-problems-in-subsurface-imaging-using-latent-space-translations-has-been-accepted-to-iclr-2025-page-facing-up-paper-computer-code",
+          title: ':tada: I finally have an ICLR paper! Thrilled to share that our paper,...',
+          description: "",
+          section: "News",},{id: "projects-project-1",
+          title: 'project 1',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
+            },},{id: "projects-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
+            },},{id: "projects-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
+            },},{id: "projects-project-4",
+          title: 'project 4',
+          description: "another without an image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
+            },},{id: "projects-project-6",
+          title: 'project 6',
+          description: "a project with no image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
+            },},{id: "projects-project-7",
+          title: 'project 7',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/7_project/";
+            },},{id: "projects-project-8",
+          title: 'project 8',
+          description: "an other project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/8_project/";
+            },},{id: "projects-project-9",
+          title: 'project 9',
+          description: "another project with an image 🎉",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/9_project/";
+            },},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%73%61%77%68%6E%65%79.%6D%65%64%68%61@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/sawhney-medha", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/medha-sawhney", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=o9Gj0dMAAAAJ&hl", "_blank");
+        },
+      },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/medha_sawhney", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
